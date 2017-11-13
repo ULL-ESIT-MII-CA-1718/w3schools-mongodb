50fce514259733aa98bc2d4fe4610888da94559b
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-11-13 14:04:39 +0000

    demo_mongodb_createcollection.js

--- demo_mongodb_createcollection.js
+++ demo_mongodb_createcollection.js
@@ -4,12 +4,16 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
+  if (err) {
+      console.log("callback: Start the deamon with 'gulp start'");
+      return null;
+  }
   // Create a new collection on a server with the specified options
   // Returns: Promise if no callback passed 
+  // See demo_mongodb_createcollection_promise.js
   db.createCollection("customers", function(err, res) {
     if (err) {
-      console.log("callback: Arranca el deamon con 'gulp start'");
+      console.log("Error in createCollection: "+err);
       return null;
     }
     console.log("Collection created!");
